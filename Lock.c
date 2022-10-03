a21df738eb28c04e17a5be4507fb029ff4862476
Jorge <jorgelisboa659@gmail.com>
2022-10-03 17:37:40 -0300

    Variables and loops created

---
+++ Lock.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+unsigned int tentativas, deslizamentos = 0;
+unsigned int arraySize = 22; //Tamanho da array/senha
+unsigned int numerosVisiveis[arraySize];
+unsigned int numerosVerdadeiros[] = { //Tipo um hashmap/json quantidade:número
+    0, 4,
+    1, 7,
+    5, 2,
+    7, 1,
+    9, 3
+};
+unsigned int senhaDigitada[22]; //Podem ser digitados 22 digitos
+
+int main() {
+    printf("Bem vindo, insira sua senha para abrir o cofre!");
+
+    while (tentativas < 5) //Looping de tentativas
+    {
+        for (size_t i = 0; i < count; i++) //Loop de deslizamentos
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
